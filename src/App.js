8b880d47b700e279d45ffb37d1907aee4b43b376
SeungPyo Hong <109272099+Sam-Ryong@users.noreply.github.com>
2024-04-14 19:38:39 +0900

    refactor: 페이지 별로 정리 및 초기 세팅
    react-router-dom' 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import MainPage from './pages/MainPage'
+import LoginPage from './pages/LoginPage';
+import UploadPage from './pages/UploadPage';
+import TrashPage from './pages/TrashPage';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index element={<MainPage />} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/Upload' element={<UploadPage />} />
+        <Route path='/Trash' element={<TrashPage />} />
+      </Routes>
     </div>
   );
 }
