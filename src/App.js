00f15472bbebe7ac96cefe4b534e007227e317ac
sam_ryong <zackinthebox@khu.ac.kr>
2024-05-07 23:27:57 +0900

    feat: Header(Navbar) created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import { Routes, Route } from 'react-router-dom';
-import MainPage from './pages/MainPage'
-import LoginPage from './pages/LoginPage';
-import UploadPage from './pages/UploadPage';
-import TrashPage from './pages/TrashPage';
+import { Routes, Route } from "react-router-dom";
 
+import MainPage from "./pages/MainPage";
+import LoginPage from "./pages/LoginPage";
+import UploadPage from "./pages/UploadPage";
+import TrashPage from "./pages/TrashPage";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route index element={<MainPage />} />
-        <Route path='/login' element={<LoginPage />} />
-        <Route path='/Upload' element={<UploadPage />} />
-        <Route path='/Trash' element={<TrashPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/Upload" element={<UploadPage />} />
+        <Route path="/Trash" element={<TrashPage />} />
+        <Route path="/Test" element={<Header />} />
       </Routes>
     </div>
   );
