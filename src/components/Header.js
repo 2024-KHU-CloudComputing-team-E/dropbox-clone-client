10a39125070f5adbc7b545c7d3ba36cb49db69b1
sam_ryong <zackinthebox@khu.ac.kr>
2024-05-08 00:47:05 +0900

    style: add search form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,6 @@
 import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -17,6 +19,15 @@ function Header() {
             <Nav.Link href="#features">Features</Nav.Link>
             <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+          </Form>
         </Container>
       </Navbar>
     </>
--- src/components/Leftbar.js
+++ src/components/Leftbar.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Leftbar() {
   return (
     <Dropdown.Menu show>
+      <Dropdown.Item eventKey="1">InstaBox</Dropdown.Item>
       <Dropdown.Item eventKey="1">메뉴 1</Dropdown.Item>
       <Dropdown.Item eventKey="2">메뉴 2</Dropdown.Item>
       <Dropdown.Item eventKey="3">메뉴 3</Dropdown.Item>
