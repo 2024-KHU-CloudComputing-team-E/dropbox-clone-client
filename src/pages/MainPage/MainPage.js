bee063c1113e1d828737a851c320cea4bd97cf1d
byeongmin-kwak <bqudmals@naver.com>
2024-05-17 23:59:59 +0900

    design 모달 창 레이아웃 생성

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import Header from "../../components/Header";
 import Leftbar from "../../components/Leftbar";
 import { AiFillCaretDown } from "react-icons/ai";
 import './MainPage.css'
+import DetailModal from '../../components/DetailModal/DetailModal';
+
 
 export default function MainPage() {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalImgSrc, setModalImgSrc] = useState('');
+
+  const openModal = (imgSrc) => {
+    setModalImgSrc(imgSrc);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setModalImgSrc('');
+  };
+
   return (
     <div>
       <Header />
@@ -21,12 +37,17 @@ export default function MainPage() {
           </button>
         </div>
         <div className="container">
-          {[...Array(20)].map((_, index) => (
-            <div key={index} className="item">
-              <img src="./logo192.png" alt={`썸네일${index + 1}`} />
-            </div>
-          ))}
-        </div>
+        {[...Array(20)].map((_, index) => (
+          <div key={index} className="item">
+            <img 
+              src="./logo192.png" 
+              alt={`썸네일${index + 1}`} 
+              onClick={() => openModal("./logo192.png")}
+            />
+          </div>
+        ))}
+      </div>
+      <DetailModal isOpen={isModalOpen} onClose={closeModal} imgSrc={modalImgSrc} />
       </div>
     </div>
   );
