273380d8cc54eb1f165432d9329a87cb2a65319a
chaekeun <chaen328@khu.ac.kr>
2024-06-13 19:23:31 +0900

    240613

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import login from './googleLoginImage.svg';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios';
+import React from "react";
+import login from "./googleLoginImage.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios";
 
 const BASE_URL = process.env.REACT_APP_BASE_URL;
 
@@ -12,7 +12,7 @@ export default function LoginPage() {
       const response = await axios.get(`${BASE_URL}/api/login/google`);
       console.log(response.data);
     } catch (error) {
-      console.error('Error logging in with Google:', error);
+      console.error("Error logging in with Google:", error);
     }
   };
 
@@ -22,12 +22,12 @@ export default function LoginPage() {
         <h1 className="w-100">InstaBox</h1>
       </div>
       <div className="w-25">
-        <img 
-          src={login} 
-          alt='구글 로그인' 
+        <img
+          src={login}
+          alt="구글 로그인"
           className="w-100"
           onClick={handleLoginClick}
-          style={{ cursor: 'pointer' }}
+          style={{ cursor: "pointer" }}
         />
       </div>
     </div>
