015ca954efffc7e0d7228004aa12e4b1e3fef17e
chaekeun <chaen328@khu.ac.kr>
2024-06-16 03:30:44 +0900

    240616 feat:upload

--- src/pages/UploadPage.js
+++ src/pages/UploadPage.js
@@ -49,26 +49,6 @@ const UploadBox = () => {
 
     const formData = new FormData();
     formData.append("file", file);
-
-    fetch("/api/uploadfile/file", {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        console.log("Upload successful", data);
-        alert("파일 업로드가 성공했습니다!");
-        window.location.reload();
-      })
-      .catch((err) => {
-        console.error("Error during file upload: ", err);
-        alert("파일 업로드 중 오류가 발생했습니다.");
-      });
   };
 
   const setFileInfo = (file) => {
@@ -116,17 +96,14 @@ const UploadBox = () => {
       method: "POST",
       body: formData,
     })
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
       .then((data) => {
         console.log("Upload successful", data);
+        alert("파일 업로드가 성공했습니다!");
+        window.location.reload();
       })
       .catch((err) => {
         console.error("Error during file upload: ", err);
+        alert("파일 업로드 중 오류가 발생했습니다.");
       });
   };
 
