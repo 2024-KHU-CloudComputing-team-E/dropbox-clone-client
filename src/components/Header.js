76564980fff6f091e4be96e602c57cdf819bc807
byeongmin-kwak <bqudmals@naver.com>
2024-06-17 01:04:51 +0900

    유저 검색 기능

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,41 @@
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Navbar from "react-bootstrap/Navbar";
+import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Header.css";
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 function Header() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [debounceTimeout, setDebounceTimeout] = useState(null);
+
+  useEffect(() => {
+    if (searchTerm) {
+      if (debounceTimeout) {
+        clearTimeout(debounceTimeout);
+      }
+      const timeout = setTimeout(() => {
+        fetchSearchResults(searchTerm);
+      }, 300);
+      setDebounceTimeout(timeout);
+    } else {
+      setSearchResults([]);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchResults = async (term) => {
+    try {
+      const response = await axios.get(`${BASE_URL}/api/search/${term}`);
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error("Failed to fetch search results", error);
+    }
+  };
+
   return (
     <>
       <Navbar className="header-navbar" bg="light" variant="light">
@@ -15,7 +46,18 @@ function Header() {
               placeholder="Search"
               className="search-bar"
               aria-label="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
             />
+            {searchResults.length > 0 && (
+              <div className="search-results">
+                {searchResults.map((result, index) => (
+                  <div key={index} className="search-result-item">
+                    {result}
+                  </div>
+                ))}
+              </div>
+            )}
           </Form>
         </Container>
       </Navbar>
